66871f1ac323285d0a43dde2108f8e73aed4ee3f
tharwerth <tharwerth@gmail.com>
2024-12-05 17:18:53 -0600

    add solution for day 2 part 1/2 (#2)
    add solution for day 2 part 1/2 (#2)

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc24/day01"
+	"aoc24/day02"
 	"fmt"
 	"os"
 	"strconv"
@@ -25,6 +26,11 @@ func main() {
 		fmt.Printf("Day 1 Solution 1: %d\n\n", day01.Solution1())
 		fmt.Printf("Day 1 Problem 2:\n%v\n\n", day01.GetProblem2())
 		fmt.Printf("Day 1 Solution 2: %d\n\n", day01.Solution2())
+	case 2:
+		fmt.Printf("Day 2 Problem 1:\n%v\n\n", day02.GetProblem1())
+		fmt.Printf("Day 2 Solution 1: %d\n\n", day02.Solution1())
+		fmt.Printf("Day 2 Problem 2:\n%v\n\n", day02.GetProblem2())
+		fmt.Printf("Day 2 Solution 2: %d\n\n", day02.Solution2())
 	default:
 		fmt.Printf("Day %d not implemented yet\n", day)
 	}
